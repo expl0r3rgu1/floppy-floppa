35af567d1bf1716f27d1a0a8dec5117dcf2a3977
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-03-07 23:00:39 +0100

    creating movingPattern method

--- src/main/obstacles/MovingObstacle.java
+++ src/main/obstacles/MovingObstacle.java
@@ -1,5 +1,44 @@
 package main.obstacles;
 
-public class MovingObstacle {
+import java.awt.Toolkit;
 
+public class MovingObstacle extends Obstacle {
+
+	int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+
+	public MovingObstacle() {
+		super();
+	}
+
+	public void movingPattern() {
+		this.position = height / 2;
+		int j = 0;
+		while (j < 5) {
+			for (int i = 1; i <= 5; i++) {
+				this.position = this.updateView(this.position, 1);
+				System.out.println(position);
+			}
+			this.sleep(1000);
+
+			for (int i = 1; i <= 5; i++) {
+				this.position = this.updateView(this.position, -1);
+				System.out.println(position);
+			}
+			this.sleep(1000);
+			j++;
+		}
+	}
+
+	private int updateView(int position, int increment) {
+		this.sleep(250);
+		return position + increment;
+	}
+
+	private void sleep(int milliseconds) {
+		try {
+			Thread.sleep(milliseconds);
+		} catch (InterruptedException ex) {
+			Thread.currentThread().interrupt();
+		}
+	}
 }
