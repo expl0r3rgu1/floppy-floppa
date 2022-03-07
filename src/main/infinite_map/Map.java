f32b40d988b1023bd9846cefaf36a819ba638c1c
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-03-07 21:50:13 +0100

    Defined the structure of the class according to UML diagram

--- src/main/infinite_map/Map.java
+++ src/main/infinite_map/Map.java
@@ -1,5 +1,48 @@
 package main.infinite_map;
 
-public class Map {
+import java.awt.Graphics2D;
+import java.util.Set;
 
+import main.obstacles.FixedObstacle;
+import main.obstacles.MovingObstacle;
+
+public class Map {
+	private Background background;
+	private FixedObstacle fixedObstacle;
+	private Set<MovingObstacle> movingObstacles;
+	
+	public Map(Background background, FixedObstacle fixedObstacle, Set<MovingObstacle> movingObstacles) {
+		super();
+		this.background = background;
+		this.fixedObstacle = fixedObstacle;
+		this.movingObstacles = movingObstacles;
+	}
+
+	public Background getBackground() {
+		return background;
+	}
+
+	public void setBackground(Background background) {
+		this.background = background;
+	}
+
+	public FixedObstacle getFixedObstacle() {
+		return fixedObstacle;
+	}
+
+	public void setFixedObstacle(FixedObstacle fixedObstacle) {
+		this.fixedObstacle = fixedObstacle;
+	}
+
+	public Set<MovingObstacle> getMovingObstacles() {
+		return movingObstacles;
+	}
+
+	public void setMovingObstacles(Set<MovingObstacle> movingObstacles) {
+		this.movingObstacles = movingObstacles;
+	}
+	
+	public void animate(Graphics2D canvas) {
+		
+	}
 }
