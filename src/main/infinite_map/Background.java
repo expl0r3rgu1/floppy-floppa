7c36fde4e862ead09860239977b3284a22e6bf8a
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-03-07 18:55:33 +0100

    Defined the structure of the class according to UML diagram

--- src/main/infinite_map/Background.java
+++ src/main/infinite_map/Background.java
@@ -1,5 +1,40 @@
 package main.infinite_map;
 
-public class Background {
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import main.utilities.Movable;
+import main.utilities.Position;
+
+public class Background extends Movable {
+	private String name;
+	private Image image;
+
+	public Background(String name, Image image, Position position) {
+		super(position);
+		this.name = name;
+		this.image = image;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	@Override
+	public void animate(Graphics2D g2d) {
+		
+	}
 
 }
