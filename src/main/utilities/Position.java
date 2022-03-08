5133f6b14547022f5076cd945ec92f53c55532ab
criicchio <cristina.zoccola01@gmail.com>
2022-03-08 16:55:20 +0100

    Coded the Position class

--- src/main/utilities/Position.java
+++ src/main/utilities/Position.java
@@ -1,5 +1,50 @@
 package main.utilities;
 
-public class Position {
+import java.awt.*;
 
+public class Position {
+	//the range of these integers is from 0 to the screen size
+	private int x; 
+	private int y;
+	
+	Position(int x, int y) {
+		if(this.validPosition(x, y)) {
+			this.x=x;
+			this.y=y;
+		}
+	}
+	
+	private boolean validPosition(int x, int y) {
+		//I get the screen size to check if the position is in the right range
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		int maxWidth = gd.getDisplayMode().getWidth();
+		int maxHeight = gd.getDisplayMode().getHeight();
+		
+		if(x==maxWidth && y==maxHeight) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public void setX(int x) {
+		if(this.validPosition(x, this.y)) {
+			this.x=x;
+		}
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public void setY(int y) {
+		if(this.validPosition(this.x, y)) {
+			this.y=y;
+		}
+	}
 }
