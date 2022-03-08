ca661ee569e36ffbecae54118a459b0fcc2043b0
criicchio <cristina.zoccola01@gmail.com>
2022-03-08 11:51:29 +0100

    made a few methods in Character class

--- src/main/character/Character.java
+++ src/main/character/Character.java
@@ -1,5 +1,45 @@
 package main.character;
 
-public class Character {
+import main.utilities.Movable;
+import main.utilities.Position;
+import main.utilities.Skin;
 
+public class Character extends Movable {
+	private Skin skin;
+	private boolean dead; //true if the character is alive
+	private boolean hit; //true if the character is hit by an obstacle
+	
+	Character(Skin skin, Position position) {
+		this.skin=skin;
+		//
+		this.dead=false;
+		this.hit=false;
+	}
+	
+	public boolean isDead() {
+		if (dead) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void die() {
+		this.dead=true;
+	}
+	
+	public boolean isHit() {
+		if (hit) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void hit() {
+		this.hit=true;
+	}
+	
 }
