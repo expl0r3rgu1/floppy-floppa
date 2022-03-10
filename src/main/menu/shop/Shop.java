9a6e42cfb660dec698876c8a837667e5ba720cd9
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-03-10 21:48:09 +0100

    Shop class created with fields and constructors

--- src/main/menu/shop/Shop.java
+++ src/main/menu/shop/Shop.java
@@ -1,5 +1,19 @@
 package main.menu.shop;
 
-public class Shop {
+import java.util.Set;
 
+import main.infinite_map.Background;
+import main.utilities.Skin;
+
+public class Shop {
+	private Integer coins;
+	private Set<PurchaseStatus<Skin>> skins;
+	private Set<PurchaseStatus<Background>> sceneries;
+	
+	public Shop(Integer coins, Set<PurchaseStatus<Skin>> skins, Set<PurchaseStatus<Background>> sceneries) {
+		this.coins = coins;
+		this.skins = skins;
+		this.sceneries = sceneries;
+	}
+	
 }
