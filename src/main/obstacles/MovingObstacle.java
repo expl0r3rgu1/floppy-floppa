1957c715a7c1100791851bbfc413b53136065811
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-03-10 19:50:18 +0100

    ValidPosition method added to check position when the obstacle moves

--- src/main/obstacles/MovingObstacle.java
+++ src/main/obstacles/MovingObstacle.java
@@ -3,44 +3,54 @@ package main.obstacles;
 import java.awt.Toolkit;
 
 public class MovingObstacle extends Obstacle {
-
 	
 	int height = Toolkit.getDefaultToolkit().getScreenSize().height;
 
 	public MovingObstacle() {
 		super();
+		this.position = height/2;
+		this.skin;
 	}
 
 	public void movingPattern() {
-		this.position = height / 2;
-		int j = 0;
-		while (j < 3) {
+		while (true) {
 			if(position == height/2) {
 				for (int i = 1; i <= 5; i++) {
 					this.position = this.updateView(this.position, 1);
 					System.out.println(position);
 				}
-				this.sleep(750);
+				this.sleep(800);
 			}else {
 				for (int i = 1; i <= 10; i++) {
 					this.position = this.updateView(this.position, -1);
 					System.out.println(position);
 				}
-				this.sleep(750);
+				this.sleep(800);
 
-				for (int i = 1; i <= 10; i++) {
+				for (int i = 1; i <= 0; i++) {
 					this.position = this.updateView(this.position, +1);
 					System.out.println(position);
 				}
-				this.sleep(750);
+				this.sleep(800);
 			}
-			j++;
+		}
+	}
+	public boolean validPosition(int pos) {
+		int maxHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
+		
+		if(pos <= maxHeight && pos >= 0) {
+			return true;
+		}else {
+			return false;
 		}
 	}
 
 	private int updateView(int position, int increment) {
 		this.sleep(250);
-		return position + increment;
+		if(validPosition(position+increment)) {
+			return position + increment;
+		}
+		return 0;
 	}
 
 	private void sleep(int milliseconds) {
