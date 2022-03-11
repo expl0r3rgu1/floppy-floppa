7227184383cdcb534f6eb20c691e2e4b2ec4d838
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-03-11 21:58:28 +0100

    done class Obstacle

--- src/main/obstacles/Obstacle.java
+++ src/main/obstacles/Obstacle.java
@@ -1,5 +1,22 @@
 package main.obstacles;
 
-public class Obstacle {
+import main.utilities.Movable;
+import main.utilities.Position;
+import main.utilities.Skin;
 
+public class Obstacle extends Movable{
+	private Skin skin;
+	
+	public Obstacle factoryObstacle(Skin skin, Position position) {
+		final Obstacle obs = new Obstacle();
+		return obs;
+	}
+	
+	public Skin getSkin() {
+		return this.skin;
+	}
+	
+	public void setSkin(Skin skin) {
+		this.skin=skin;
+	}
 }
