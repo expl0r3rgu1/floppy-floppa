872b6481fd1e65d48e515d7883334c14a8ec6a80
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-03-15 18:16:03 +0100

    Implemented constructor with a method to obtain info from file

--- src/main/menu/shop/Shop.java
+++ src/main/menu/shop/Shop.java
@@ -1,5 +1,11 @@
 package main.menu.shop;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 import java.util.Set;
 
 import main.infinite_map.Background;
@@ -9,11 +15,51 @@ public class Shop {
 	private Integer coins;
 	private Set<PurchaseStatus<Skin>> skins;
 	private Set<PurchaseStatus<Background>> sceneries;
-	
+
 	public Shop(Integer coins, Set<PurchaseStatus<Skin>> skins, Set<PurchaseStatus<Background>> sceneries) {
-		this.coins = coins;
-		this.skins = skins;
-		this.sceneries = sceneries;
+		getFileInfo();
 	}
-	
+
+	private void getFileInfo() {
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner("file.txt");
+		} finally {
+			
+		}
+		int counter = 0;
+		while (scanner.hasNextLine()) {
+			Scanner nextscanner = new Scanner(scanner.nextLine());
+			if (counter == 0) {
+				while (nextscanner.hasNext()) {
+					String word = nextscanner.next();
+					this.coins = Integer.parseInt(word);
+				}
+				counter++;
+				continue;
+			} else if (counter == 1) {
+				while (nextscanner.hasNext()) {
+					PurchaseStatus<Skin> purchaseStatus = new PurchaseStatus<>();
+					String word = nextscanner.next();
+					if (word.equals("1")) {
+						purchaseStatus.purchase();
+					}
+					this.skins.add(purchaseStatus);
+				}
+				counter++;
+				continue;
+			} else if (counter == 2) {
+				while (nextscanner.hasNext()) {
+					PurchaseStatus<Background> purchaseStatus = new PurchaseStatus<>();
+					String word = nextscanner.next();
+					if (word.equals("1")) {
+						purchaseStatus.purchase();
+					}
+					this.sceneries.add(purchaseStatus);
+				}
+			}
+			break;
+		}
+	}
+
 }
