4f8a06c3e916c6fcd60b236e8b42403ba2646ff4
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-03-18 18:43:18 +0100

    Created Leaderboard class according to UML

--- src/main/menu/leaderboard/Leaderboard.java
+++ src/main/menu/leaderboard/Leaderboard.java
@@ -1,5 +1,28 @@
 package main.menu.leaderboard;
 
-public class Leaderboard {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class Leaderboard {
+	private File leaderboardRecord;
+	private ArrayList<Player> leaderboard;
+
+	public Player getBestPlayer() {
+		this.sortLeaderboard();
+		return leaderboard.get(0);
+	}
+
+	public ArrayList<Player> getLeaderboard() {
+		this.sortLeaderboard();
+		return leaderboard;
+	}
+
+	public void sortLeaderboard() {
+		leaderboard.sort((a, b) -> a.getPersonalBest() - b.getPersonalBest());
+	}
+
+	public void update(Player player) {
+		
+	}
 }
