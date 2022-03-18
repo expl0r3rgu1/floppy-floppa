87315a910b5403ad8a047e0735b28daf923dbaf5
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-03-18 18:06:56 +0100

    Created Player class according to UML

--- src/main/menu/leaderboard/Player.java
+++ src/main/menu/leaderboard/Player.java
@@ -1,5 +1,31 @@
 package main.menu.leaderboard;
 
 public class Player {
+	private String nickname;
+	private int personalBest;
 
+	public Player(String nickname, int personalBest) {
+		this.nickname = nickname;
+		if (personalBest >= 0) {
+			this.personalBest = personalBest;
+		}
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public int getPersonalBest() {
+		return personalBest;
+	}
+
+	public void setPersonalBest(int personalBest) {
+		if (personalBest >= 0) {
+			this.personalBest = personalBest;
+		}
+	}
 }
