89e5b2db31de1239754217ed21c9d3bcf306cd5c
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-03-23 18:31:47 +0100

    Created ShopGUI class

---
+++ src/main/menu/shop/ShopGUI.java
@@ -0,0 +1,12 @@
+package main.menu.shop;
+
+import javax.swing.JFrame;
+
+public class ShopGUI extends JFrame {
+
+	private static final long serialVersionUID = -7631305128085484196L;
+
+	public ShopGUI() {
+
+	}
+}
