d8b0964c406ad6305db416442aa25ab86121eb3e
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-03-23 22:23:09 +0100

    made Interface Menu and started MainMenu

--- src/main/menu/MainMenu.java
+++ src/main/menu/MainMenu.java
@@ -1,5 +1,37 @@
 package main.menu;
 
-public class MainMenu {
+import java.awt.Graphics2D;
 
+import javax.swing.JPanel;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.Rectangle;
+
+public class MainMenu {
+	
+	private enum STATES{
+			MENU, GAME
+	};
+	
+	private STATES condition=STATES.MENU;
+	public static final Dimension size= Toolkit.getDefaultToolkit().getScreenSize();
+	//JPanel buttons;
+	private Rectangle play = new Rectangle((int)(size.width/2 + 100),150,100,50);
+	private Rectangle leaderboard = new Rectangle((int)(size.width/2 + 100),150,100,50);
+	private Rectangle clear = new Rectangle((int)(size.width/2 + 100),150,100,50);
+	private Rectangle shop = new Rectangle((int)(size.width/2 + 100),150,100,50);
+	private Rectangle quit = new Rectangle((int)(size.width/2 + 100),150,100,50);
+	private Rectangle tutorials = new Rectangle((int)(size.width/2 + 100),150,100,50);
+	
+	public void render(Graphics2D a) {
+		
+		Font font = new Font("arial", Font.BOLD, 50);
+		a.setFont(font);
+		a.setColor(Color.white);
+		a.drawString("Floppy Floppa", (int) (size.getWidth()/2) , 100);
+		
+		a.draw(buttons);
+	}
 }
