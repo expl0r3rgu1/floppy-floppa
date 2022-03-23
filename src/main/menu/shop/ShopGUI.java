842d5cf911a1fe6b88896414201014592a9dc0db
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-03-23 19:15:24 +0100

    Changed from extends JFrame to extends JPanel

--- src/main/menu/shop/ShopGUI.java
+++ src/main/menu/shop/ShopGUI.java
@@ -1,12 +1,12 @@
 package main.menu.shop;
 
-import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class ShopGUI extends JFrame {
+public class ShopGUI extends JPanel{
 
 	private static final long serialVersionUID = -7631305128085484196L;
 
-	public ShopGUI() {
-
+	public ShopGUI(int size) {
+		
 	}
 }
