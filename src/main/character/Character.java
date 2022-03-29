952e890a5862a9570ff9d5f815b55b0146a668b8
criicchio <cristina.zoccola01@gmail.com>
2022-03-29 22:05:13 +0200

    Added some fields

--- src/main/character/Character.java
+++ src/main/character/Character.java
@@ -4,11 +4,17 @@ import main.utilities.Movable;
 import main.utilities.Position;
 import main.utilities.Skin;
 import java.awt.*;
+import java.util.Timer;
 
 public class Character extends Movable {
 	private Skin skin;
 	private boolean dead; //true if the character is dead
 	private boolean hit; //true if the character is hit by an obstacle
+	private int velocity; //it will be used to move the character
+	private int gravity; //it will determine if the character goes down (default) or up if it jumps
+	private static int DELAY=1; //a constant used to make the typical effect of the bird's jump in flappy bird
+	private Timer timer;
+	
 	
 	Character(Position position, Skin skin) {
 		super(position);
