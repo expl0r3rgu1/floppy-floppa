6e8596b0966d078f5af134c51b84deac5b3209d2
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-03-31 16:54:16 +0200

    Changed class name for better code readability

--- src/main/menu/shop/BuyButton.java
+++ src/main/menu/shop/BuyButton.java
@@ -2,12 +2,12 @@ package main.menu.shop;
 
 import javax.swing.JButton;
 
-public class CustomJButton extends JButton {
+public class BuyButton extends JButton {
 
 	private static final long serialVersionUID = 3550487230752126610L;
 	private Object object;
 
-	public CustomJButton(String name, Object object) {
+	public BuyButton(String name, Object object) {
 		super(name);
 		this.object = object;
 	}
