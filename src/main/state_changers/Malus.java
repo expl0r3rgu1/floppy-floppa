b5b906f13fbd2d110a538f82c61ce1b8e42fb84c
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-01 16:03:17 +0200

    began Maluses

--- src/main/state_changers/Malus.java
+++ src/main/state_changers/Malus.java
@@ -1,5 +1,20 @@
 package main.state_changers;
 
 public abstract class Malus {
-
+	
+	//Lose coins
+	private Integer coins;
+	public Malus1;
+	
+	public Integer getCoins() {
+		return this.coins;
+	}
+	
+	Malus1{
+		
+	}
+	
+	//animate
+	
+	//random number
 }
