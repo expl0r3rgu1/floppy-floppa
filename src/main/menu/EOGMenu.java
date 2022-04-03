9b66baf3a41df0a6ab1298455ed546cdcd609886
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-03 12:25:07 +0200

    Created updateCoins method

--- src/main/menu/EOGMenu.java
+++ src/main/menu/EOGMenu.java
@@ -1,5 +1,14 @@
 package main.menu;
 
-public class EOGMenu {
+public class EOGMenu implements Menu {
 
+	public EOGMenu() {
+
+	}
+
+	public static int updateCoins(int meters) {
+		int coinsWon = (int) Math.floor(meters / 5);
+
+		return coinsWon;
+	}
 }
