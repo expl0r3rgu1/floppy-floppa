47c7b73745e2a9656f2a6e7a4715fed7df319f42
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-03 12:01:36 +0200

    Created PricedBackground class

---
+++ src/main/infinite_map/PricedBackground.java
@@ -0,0 +1,20 @@
+package main.infinite_map;
+
+import java.awt.Image;
+
+public class PricedBackground extends Background{
+	private int price;
+
+	public PricedBackground(String name, Image image, int price) {
+		super(name, image);
+		this.price = price;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
