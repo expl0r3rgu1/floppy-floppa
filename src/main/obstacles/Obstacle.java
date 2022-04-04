f01206291d99762ed82318d2e8167f8a943eddb1
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-04 16:22:04 +0200

    corrected Obstacle.java

--- src/main/obstacles/Obstacle.java
+++ src/main/obstacles/Obstacle.java
@@ -6,16 +6,16 @@ import main.utilities.Position;
 import main.utilities.Skin;
 import java.awt.Graphics2D;
 
-public abstract class Obstacle extends Movable{
+public class Obstacle extends Movable{
 	private Skin skin;
 	
-	private Obstacle(Position position, Skin skin) {
+	protected Obstacle(Position position, Skin skin) {
 		super(position);
 		this.skin=skin;
 	}
 	
 	public static Obstacle factoryObstacle(Skin skin, Position position) {
-		return new Obstacle(position, skin) {};
+		return new Obstacle(position, skin);
 	}
 	
 	public Skin getSkin() {
