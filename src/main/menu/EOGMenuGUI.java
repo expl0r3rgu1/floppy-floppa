cdf6603db40b40401ab792d7038c17f1bc88393a
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-04 17:24:29 +0200

    Created setDimensionObject method to set objects in their right position in the gridBagLayout

--- src/main/menu/EOGMenuGUI.java
+++ src/main/menu/EOGMenuGUI.java
@@ -2,8 +2,10 @@ package main.menu;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
+import java.awt.Insets;
 import java.awt.Toolkit;
 import java.io.IOException;
 
@@ -20,7 +22,7 @@ public class EOGMenuGUI extends JPanel{
 		this.setLayout(new GridBagLayout());
 
 		try {
-			background = ImageIO.read(getClass().getResource("/test/Background.png"));
+			background = ImageIO.read(getClass().getResource("/resources/images/Background.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -35,6 +37,16 @@ public class EOGMenuGUI extends JPanel{
 	
 	private Image scale(Image image, Dimension dim) {
 		return image.getScaledInstance((int) dim.getWidth(), (int) dim.getHeight(), Image.SCALE_DEFAULT);
+	}
+	
+	private GridBagConstraints setDimensionObject(int gridx, int gridy, int ipadx, int ipady, Insets i) {
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = gridx;
+		c.gridy = gridy;
+		c.ipadx = ipadx;
+		c.ipady = ipady;
+		c.insets = i;
+		return c;
 	}
 	
 }
