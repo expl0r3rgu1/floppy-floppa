11d5413e3c6a8f0706175b564e738302903420e4
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-04 17:07:33 +0200

    Created EOGMenuGUI constructor

--- src/main/menu/EOGMenuGUI.java
+++ src/main/menu/EOGMenuGUI.java
@@ -1,5 +1,28 @@
 package main.menu;
 
-public class EOGMenuGUI {
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class EOGMenuGUI extends JPanel{
+	
+	private static final long serialVersionUID = -6834009314888626973L;
+	private static final Dimension SIZE = Toolkit.getDefaultToolkit().getScreenSize();
+	private Image background;
+	
+	public EOGMenuGUI() {
+		this.setLayout(new GridBagLayout());
+
+		try {
+			background = ImageIO.read(getClass().getResource("/test/Background.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
