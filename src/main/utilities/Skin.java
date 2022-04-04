59b88b00d1d1736599762a8af1a42766ec226ef7
criicchio <cristina.zoccola01@gmail.com>
2022-04-04 15:53:37 +0200

    Implemented Skin class

--- src/main/utilities/Skin.java
+++ src/main/utilities/Skin.java
@@ -1,5 +1,32 @@
 package main.utilities;
 
-public class Skin {
+import java.awt.Image;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+public class Skin {
+	private String name;
+	private Image image;
+	
+	Skin(String name, Image image){
+		this.name = name;
+		this.image = image;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
 }
