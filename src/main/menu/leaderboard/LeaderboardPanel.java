a376a6182fc1baac2dae672bf0ed222fe2f38f52
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-04 18:13:14 +0200

    Added JPanel to add to scrollable pane that contains scores

--- src/main/menu/leaderboard/LeaderboardPanel.java
+++ src/main/menu/leaderboard/LeaderboardPanel.java
@@ -1,11 +1,13 @@
 package main.menu.leaderboard;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Insets;
 import java.awt.Toolkit;
@@ -21,23 +23,38 @@ public class LeaderboardPanel extends JPanel {
 	private static final long serialVersionUID = -2850654943551437120L;
 	private Leaderboard leaderboard;
 	private final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
-	
+
 	Image background;
 	JLabel title;
-	
 
 	public LeaderboardPanel() {
 		this.setPreferredSize(SCREEN_SIZE);
-		
-		try {
-			background = ImageIO.read(getClass().getResource("/resources/images/menuBackground.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
+
+		leaderboard = new Leaderboard();
+
+		background = getImageResource("menuBackground.png");
+
+		// TITLE
 		title = new JLabel("LEADERBOARD", SwingConstants.CENTER);
 		title.setFont(getFontResource("pixel.TTF").deriveFont(50f));
-		this.add(title, getConstraints(0, 0, new Insets(0, 0, (int) (SCREEN_SIZE.getWidth() * 0.02), 0), 0, 0, GridBagConstraints.PAGE_START));
+		this.add(title, getConstraints(0, 0, new Insets(0, 0, (int) (SCREEN_SIZE.getWidth() * 0.02), 0), 0, 0,
+				GridBagConstraints.PAGE_START));
+
+		// SCROLLABLE PANE
+		JPanel scoresPanel = new JPanel(new GridLayout(leaderboard.getLeaderboard().size(), 1));
+		scoresPanel.setOpaque(false);
+
+		int placeNumber = 1;
+		for (var player : leaderboard.getLeaderboard()) {
+			JLabel score = new JLabel(
+					placeNumber + " - " + player.getNickname() + " - " + player.getPersonalBest() + " m",
+					SwingConstants.CENTER);
+			score.setFont(getFontResource("arcade.ttf").deriveFont(20f));
+			score.setForeground(Color.white);
+			scoresPanel.add(score);
+
+			placeNumber++;
+		}
 	}
 
 	@Override
@@ -62,7 +79,7 @@ public class LeaderboardPanel extends JPanel {
 
 		return constraints;
 	}
-	
+
 	public Image getImageResource(String imageName) {
 		try {
 			return ImageIO.read(getClass().getResource("/resources/images/" + imageName));
@@ -72,10 +89,11 @@ public class LeaderboardPanel extends JPanel {
 
 		return null;
 	}
-	
+
 	public Font getFontResource(String fontName) {
 		try {
-			return Font.createFont(Font.TRUETYPE_FONT, new File(getClass().getResource("/resources/fonts/" + fontName).getFile()));
+			return Font.createFont(Font.TRUETYPE_FONT,
+					new File(getClass().getResource("/resources/fonts/" + fontName).getFile()));
 		} catch (FontFormatException | IOException e) {
 			e.printStackTrace();
 		}
