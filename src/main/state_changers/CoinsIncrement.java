29a39df7dc241ce73b07be24dbf4bc607e5c0109
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-05 13:31:53 +0200

    Added main method: gainCoins

--- src/main/state_changers/CoinsIncrement.java
+++ src/main/state_changers/CoinsIncrement.java
@@ -1,12 +1,23 @@
 package main.state_changers;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.util.Random;
+
 import main.utilities.Position;
 import main.utilities.Skin;
 
 public class CoinsIncrement extends Booster {
+	
+	private Random rand = new Random();
+	private static final Dimension SIZE = Toolkit.getDefaultToolkit().getScreenSize();
 
 	public CoinsIncrement(Position position, Skin skin) {
 		super(position, skin);
+	}
+	
+	public Integer gainCoins() {
+		return (int) rand.nextInt(100);
 	}
 
 }
