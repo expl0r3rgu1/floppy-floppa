ee76f9e24bae309af894fdd039901142fa40a420
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-05 13:02:15 +0200

    corrected the constructor

--- src/main/state_changers/StateChanger.java
+++ src/main/state_changers/StateChanger.java
@@ -1,9 +1,16 @@
 package main.state_changers;
 
 import main.utilities.Skin;
+import main.utilities.Movable;
+import main.utilities.Position;
 
-public abstract class StateChanger {
+public abstract class StateChanger extends Movable{
 	Skin skin;
+	
+	public StateChanger(Position position, Skin skin) {
+		super(position);
+		this.skin=skin;
+	}
 	
 	public Skin getSkin() {
 		return this.skin;
