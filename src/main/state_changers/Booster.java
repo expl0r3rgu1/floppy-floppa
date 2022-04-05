4b5435a547d0593deacca823c220850bd5055e24
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-05 12:48:22 +0200

    Implemented Booster class

--- src/main/state_changers/Booster.java
+++ src/main/state_changers/Booster.java
@@ -1,5 +1,15 @@
 package main.state_changers;
 
-public abstract class Booster {
+import main.utilities.Position;
+import main.utilities.Skin;
 
+public class Booster extends StateChanger{
+
+	public Booster(Position position, Skin skin) {
+		super(position, skin);
+	}
+	
+	public static Booster factoryBooster (Position position, Skin skin) {
+		return new Booster(position, skin);
+	}
 }
