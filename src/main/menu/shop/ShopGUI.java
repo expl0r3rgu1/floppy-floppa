3408726261816dc4feda9b10638a6aae30226197
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-05 14:53:13 +0200

    Removed placeholder method argument

--- src/main/menu/shop/ShopGUI.java
+++ src/main/menu/shop/ShopGUI.java
@@ -45,11 +45,11 @@ public class ShopGUI extends JPanel {
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawImage(this.scale(background, SIZE), 0, 0, this);
+		g.drawImage(this.scale(background), 0, 0, this);
 	}
 
-	private Image scale(Image image, Dimension dim) {
-		return image.getScaledInstance((int) dim.getWidth(), (int) dim.getHeight(), Image.SCALE_DEFAULT);
+	private Image scale(Image image) {
+		return image.getScaledInstance((int) SIZE.getWidth(), (int) SIZE.getHeight(), Image.SCALE_DEFAULT);
 	}
 
 	private JLabel imageCreation(String fileName) {
