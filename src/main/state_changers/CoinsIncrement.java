f8c57717a833bcc944c1d0c75aa61d0183fe3f05
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-05 13:30:32 +0200

    Created CoinsIncrement class

---
+++ src/main/state_changers/CoinsIncrement.java
@@ -0,0 +1,12 @@
+package main.state_changers;
+
+import main.utilities.Position;
+import main.utilities.Skin;
+
+public class CoinsIncrement extends Booster {
+
+	public CoinsIncrement(Position position, Skin skin) {
+		super(position, skin);
+	}
+
+}
