f08716f0f3f17987466418c671daf04bed71a541
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-05 13:33:42 +0200

    Did the override of animate method

--- src/main/state_changers/Booster.java
+++ src/main/state_changers/Booster.java
@@ -3,13 +3,13 @@ package main.state_changers;
 import main.utilities.Position;
 import main.utilities.Skin;
 
-public class Booster extends StateChanger{
+public class Booster extends StateChanger {
 
 	public Booster(Position position, Skin skin) {
 		super(position, skin);
 	}
-	
-	public static Booster factoryBooster (Position position, Skin skin) {
+
+	public static Booster factoryBooster(Position position, Skin skin) {
 		return new Booster(position, skin);
 	}
 }
--- src/main/state_changers/CoinsIncrement.java
+++ src/main/state_changers/CoinsIncrement.java
@@ -1,6 +1,7 @@
 package main.state_changers;
 
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.Toolkit;
 import java.util.Random;
 
@@ -8,16 +9,21 @@ import main.utilities.Position;
 import main.utilities.Skin;
 
 public class CoinsIncrement extends Booster {
-	
+
 	private Random rand = new Random();
 	private static final Dimension SIZE = Toolkit.getDefaultToolkit().getScreenSize();
 
 	public CoinsIncrement(Position position, Skin skin) {
 		super(position, skin);
 	}
-	
+
 	public Integer gainCoins() {
 		return (int) rand.nextInt(100);
 	}
 
+	@Override
+	public void animate(Graphics2D canvas) {
+		canvas.drawImage(this.getSkin().getImage(), this.position.getX(), this.position.getY(),
+				SIZE.getWidth() * 3 / 100, SIZE.getWidth() * 3 / 100, null);
+	}
 }
