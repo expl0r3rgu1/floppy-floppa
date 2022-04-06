d37e6376e8b51d150bc697edf71d79bde79c9bfb
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-06 20:48:47 +0200

    Created new Immortality class

---
+++ src/main/state_changers/Immortality.java
@@ -0,0 +1,23 @@
+package main.state_changers;
+
+import java.awt.Graphics2D;
+
+import main.utilities.Position;
+import main.utilities.Skin;
+
+public class Immortality extends Booster{
+
+	public Immortality(Position position, Skin skin) {
+		super(position, skin);
+	}
+
+	@Override 
+	public void changeState() {
+		
+	}
+	
+	@Override
+	public void animate(Graphics2D canvas) {
+		
+	}
+}
