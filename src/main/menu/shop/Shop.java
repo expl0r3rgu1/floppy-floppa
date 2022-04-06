48f127e86cd087d8d2d3d4f0be1c38638f87f677
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-06 11:27:44 +0200

    Changed package of PricedBackground class

--- src/main/menu/shop/Shop.java
+++ src/main/menu/shop/Shop.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import main.infinite_map.PricedBackground;
+import main.utilities.PricedBackground;
 import main.utilities.PricedSkin;
 
 enum TYPE {
--- src/main/utilities/PricedBackground.java
+++ src/main/utilities/PricedBackground.java
@@ -1,4 +1,4 @@
-package main.infinite_map;
+package main.utilities;
 
 import java.awt.Image;
 
