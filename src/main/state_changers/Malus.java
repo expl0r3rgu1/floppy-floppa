2ca85a591659314e59791ed7741b301cb44d7cba
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-06 18:16:23 +0200

    corrected animate and changestate implementation in Malus

--- src/main/state_changers/Malus.java
+++ src/main/state_changers/Malus.java
@@ -1,16 +1,27 @@
 package main.state_changers;
 
+import java.awt.Graphics2D;
+
 import main.utilities.Position;
 import main.utilities.Skin;
 
 public class Malus extends StateChanger {
-	
+
 	protected Malus(Position position, Skin skin) {
 		super(position, skin);
 	}
-	
+
 	public static Malus factoryMalus(Position position, Skin skin) {
 		return new Malus(position, skin);
 	}
 
+	@Override
+	public void changeState() {
+		// TO BE IMPLEMENTED IN SUB-CLASSES
+	}
+
+	@Override
+	public void animate(Graphics2D canvas) {
+		// TO BE IMPLEMENTED IN SUB-CLASSES
+	}
 }
