9aa34d75a00a95d913d443a9437f1b3cc52e8b02
criicchio <cristina.zoccola01@gmail.com>
2022-04-08 18:41:25 +0200

    Commented previous implementation of jump sistem

--- src/main/character/Character.java
+++ src/main/character/Character.java
@@ -19,16 +19,16 @@ public class Character extends Movable implements ActionListener {
 	private Skin skin;
 	private boolean dead; //true if the character is dead
 	private boolean hit; //true if the character is hit by an obstacle
-	private int velocity; //it will be used to move the character
+	/*private int velocity; //it will be used to move the character
 	private int gravity; //it will determine if the character goes down (default) or up if it jumps
 	private Timer timer;
-	private int initialY;
+	private int initialY;*/
 	
-	private final int DELAY=1; /*a constant used to make the typical effect 
-										of the bird's jump in flappy bird*/
-	private final int GO_UP=-9; 
+	//private final int DELAY=1; //a constant used to make the typical effect 
+										//of the bird's jump in flappy bird
+	/*private final int GO_UP=-9; 
 	private final int GO_DOWN=6;
-	private final int TIMER_DELAY=60;
+	private final int TIMER_DELAY=60;*/
 	
 	
 	public Character(Position position, Skin skin) {
@@ -37,10 +37,10 @@ public class Character extends Movable implements ActionListener {
 		this.skin=skin;
 		this.dead=false;
 		this.hit=false;
-		this.velocity=0;
+		/*this.velocity=0;
 		this.gravity=GO_DOWN;
 		this.timer = new Timer(TIMER_DELAY, this);
-		this.timer.start();
+		this.timer.start();*/
 	}
 	
 	public boolean isDead() {
@@ -68,7 +68,7 @@ public class Character extends Movable implements ActionListener {
 	}
 	
 	public void jump() {
-		this.gravity = GO_UP;
+		//this.gravity = GO_UP;
 		//to-do: add rotation
 	}
 	
@@ -189,12 +189,12 @@ public class Character extends Movable implements ActionListener {
 		g2D.drawImage(this.getSkin().getImage(), x, y, width, height, null);
 	}
 	
-	@Override
+	/*@Override
 	public void actionPerformed(ActionEvent e) {
 		this.gravity += this.DELAY;
 		this.velocity += this.gravity;
 		
 		this.getPosition().setY(this.getPosition().getY() + this.gravity); 
-	}
+	}*/
 	
 }
