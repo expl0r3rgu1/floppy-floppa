fc91e71b50b3249f6369136614316b6e2dd972a6
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-11 16:44:30 +0200

    Created getPixelsFromPercentage method

--- src/main/utilities/CommonMethods.java
+++ src/main/utilities/CommonMethods.java
@@ -1,5 +1,7 @@
 package main.utilities;
 
 public class CommonMethods {
-
+	public static int getPixelsFromPercentage(int percentage) {
+		return (int) Constants.SCREEN_SIZE.getWidth() * (percentage / 100);
+	}
 }
