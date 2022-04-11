361ff9e548d26a99b2c0c6aa8ed4bf24f031a4da
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-11 15:32:45 +0200

    Added SAVINGS_FILE_PATH constant

--- src/main/utilities/Constants.java
+++ src/main/utilities/Constants.java
@@ -6,4 +6,5 @@ import java.awt.Toolkit;
 public class Constants {
 	public static int SPEED = 60;
 	public static final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
+	public static final String SAVINGS_FILE_PATH = "savings";
 }
