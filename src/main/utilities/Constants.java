5e35bd26615311db5420156e65756fca2f41ca35
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-11 15:36:42 +0200

    Added CHARACTER_ANGLE_DEGREES constant

--- src/main/utilities/Constants.java
+++ src/main/utilities/Constants.java
@@ -8,4 +8,5 @@ public class Constants {
 	public static final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
 	public static final String SAVINGS_FILE_PATH = "savings";
 	public static final int CHANGED_STATE_TIME = 3000;
+	public static final int CHARACTER_ANGLE_DEGREES = 15;
 }
