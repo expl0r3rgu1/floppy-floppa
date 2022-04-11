2eec256be720663ed49b87237abbcf014ce778e3
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-11 16:09:27 +0200

    Added GBCSimplified constructor

--- src/main/utilities/GBCSimplified.java
+++ src/main/utilities/GBCSimplified.java
@@ -1,7 +1,15 @@
 package main.utilities;
 
 import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class GBCSimplified extends GridBagConstraints {
-
+	public GBCSimplified(int gridx, int gridy, int ipadx, int ipady, Insets insets) {
+		super();
+		this.gridx = gridx;
+		this.gridy = gridy;
+		this.ipadx = ipadx;
+		this.ipady = ipady;
+		this.insets = insets;
+	}
 }
