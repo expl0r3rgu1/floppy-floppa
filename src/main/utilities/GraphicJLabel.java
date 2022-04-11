9c9a076a62efa0ea7eac8bb4a495bae0ad758539
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-11 19:53:38 +0200

    Replaced SIZE with Constants.SCREEN_SIZE in GraphicJLabel class

--- src/main/utilities/GraphicJLabel.java
+++ src/main/utilities/GraphicJLabel.java
@@ -11,26 +11,28 @@ import java.io.IOException;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 
-public class GraphicJLabel extends JLabel{
+public class GraphicJLabel extends JLabel {
 
 	private static final long serialVersionUID = 3550487230752126610L;
-	private static final Dimension SIZE = Toolkit.getDefaultToolkit().getScreenSize();
-	
-	public GraphicJLabel(String name ,Color backgroundColor, Color borderColor, String font, int fontAppearance) {
+
+	public GraphicJLabel(String name, Color backgroundColor, Color borderColor, String font, int fontAppearance) {
 		super(name, CENTER);
 		this.setOpaque(true);
 		this.setBackground(backgroundColor);
 		this.setBorder(BorderFactory.createLineBorder(borderColor, 4, true));
-		this.setFont(new Font(font, fontAppearance, (int) SIZE.getWidth() / 70));
+		this.setFont(new Font(font, fontAppearance, (int) Constants.SCREEN_SIZE.getWidth() / 70));
 	}
-	
-	public GraphicJLabel(String name ,Color backgroundColor, Color borderColor, String font, float fontSize) {
+
+	public GraphicJLabel(String name, Color backgroundColor, Color borderColor, String font, float fontSize) {
 		super(name, CENTER);
 		this.setOpaque(true);
 		this.setBackground(backgroundColor);
 		this.setBorder(BorderFactory.createLineBorder(borderColor, 4, true));
 		try {
-			this.setFont(Font.createFont(Font.TRUETYPE_FONT, new File(getClass().getResource("/resources/fonts/" + font).getFile())).deriveFont(fontSize));
+			this.setFont(Font
+					.createFont(Font.TRUETYPE_FONT,
+							new File(getClass().getResource("/resources/fonts/" + font).getFile()))
+					.deriveFont(fontSize));
 		} catch (FontFormatException | IOException e) {
 			e.printStackTrace();
 		}
