2df13d564a6e571963287305f5ce2363646bafe7
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-13 16:59:44 +0200

    Added final modifier to SPEED constant

--- src/main/utilities/Constants.java
+++ src/main/utilities/Constants.java
@@ -4,7 +4,7 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 
 public class Constants {
-	public static int SPEED = 60;
+	public static final int SPEED = 60;
 	public static final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
 	public static final String SAVINGS_FILE_PATH = "savings";
 	public static final int CHANGED_STATE_TIME = 3000;
