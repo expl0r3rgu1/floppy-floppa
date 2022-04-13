789404cbc7ffd1688516ce34327546314d57bf90
criicchio <cristina.zoccola01@gmail.com>
2022-04-13 16:06:04 +0200

    Removed import that now are unused

--- src/main/character/Character.java
+++ src/main/character/Character.java
@@ -10,12 +10,8 @@ import main.utilities.Position;
 import main.utilities.Skin;
 
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
-import java.awt.image.BufferedImage;
 
 import javax.swing.Timer;
 
