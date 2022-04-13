4455f968f048148d0c1615dd94260284489f3381
criicchio <cristina.zoccola01@gmail.com>
2022-04-13 20:37:20 +0200

    Removed validPosition()

--- src/main/utilities/Position.java
+++ src/main/utilities/Position.java
@@ -6,32 +6,15 @@ public class Position {
 	private int y;
 	
 	public Position(int x, int y) {
-		if(this.validPosition(x, y)) {
-			this.x=x;
-			this.y=y;
-		}
+		this.x = x;
+		this.y = y;
 	}
-	
-	private boolean validPosition(int x, int y) {
-		
-		if(x <= Constants.SCREEN_SIZE.getWidth() 
-			&& y <= Constants.SCREEN_SIZE.getHeight()) {
-			
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	
 	public int getX() {
 		return this.x;
 	}
 	
 	public void setX(int x) {
-		if(this.validPosition(x, this.y)) {
-			this.x=x;
-		}
+		this.x = x;
 	}
 	
 	public int getY() {
@@ -39,8 +22,6 @@ public class Position {
 	}
 	
 	public void setY(int y) {
-		if(this.validPosition(this.x, y)) {
-			this.y=y;
-		}
+		this.y = y;
 	}
 }
