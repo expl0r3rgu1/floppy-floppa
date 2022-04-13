47ecdff8440d9880c486e53d531a516e4a9f1090
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-13 18:09:02 +0200

    Removed magic numbers

--- src/main/obstacles/MovingObstacle.java
+++ src/main/obstacles/MovingObstacle.java
@@ -15,25 +15,33 @@ public class MovingObstacle extends Obstacle implements ActionListener {
 	private Timer timer;
 	private int shift;
 	private int counter;
+	private int firstPositionShift;
+	private int PositionShift;
+	private int upShift;
+	private int downShift;
 
 	public MovingObstacle(Position position, Skin skin) {
 		super(position, skin);
 		counter = 0;
-		this.timer = new Timer(300, this);
+		firstPositionShift = 5;
+		PositionShift = 10;
+		upShift = 1;
+		downShift = -1;
+		this.timer = new Timer(Constants.CHANGE_DIRECTION_TIMEOUT, this);
 	}
 
 	public void movingPattern() {
 		while (true) {
 			if (counter == 0) {
 
-				this.movingPatternSupport(5, 1);
+				this.movingPatternSupport(this.firstPositionShift, this.upShift);
 				counter++;
 
 			} else {
 
-				this.movingPatternSupport(10, -1);
+				this.movingPatternSupport(this.PositionShift, this.downShift);
 
-				this.movingPatternSupport(10, 1);
+				this.movingPatternSupport(this.PositionShift, this.upShift);
 			}
 		}
 	}
