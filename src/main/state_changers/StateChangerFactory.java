c1612ed12298c322d11ca4ec85a7cc2db1ec2d00
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-15 22:43:33 +0200

    created an interface for factory of StateChangers

---
+++ src/main/state_changers/StateChangerFactory.java
@@ -0,0 +1,9 @@
+package main.state_changers;
+
+import main.utilities.Position;
+import main.utilities.Skin;
+
+public interface StateChangerFactory {
+	public Malus MalusFactory(Position position, Skin skin);
+	public Booster BoosterFactory();
+}
