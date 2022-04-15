08ed69dbfd1944a3732466768ac401012caa01f7
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-15 13:43:36 +0200

    changed SIZE to Constants.SCREEN_SIZE and added import of Position

--- src/main/state_changers/BlindBlock.java
+++ src/main/state_changers/BlindBlock.java
@@ -12,16 +12,18 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+
+import main.utilities.Constants;
+import main.utilities.Position;
 import main.utilities.Skin;
 
-public class BlindBlock extends Malus implements ActionListener{
-	
-	//Black Block
-	public static final Dimension SIZE = Toolkit.getDefaultToolkit().getScreenSize();
+public class BlindBlock extends Malus implements ActionListener {
+
+	// Black Block
 	private String blindBlock;
 	private StainPanel stain;
 	Timer timer = new Timer(300, this);
-	
+
 	public BlindBlock(Position position, Skin skin) {
 		super(position, skin);
 		blindBlock = "blindBlock";
@@ -31,18 +33,19 @@ public class BlindBlock extends Malus implements ActionListener{
 	public Object changeState() {
 		timer.start();
 		play.add(stain);
-		
+
 		return null;
 	}
-	
+
 	public void animate(Graphics2D canvas) {
-		canvas.drawImage(getSkin().getImage(), getPosition().getX(), getPosition().getY(), SIZE.getWidth() * 3 / 100, SIZE.getWidth() * 3 / 100, null);
+		canvas.drawImage(getSkin().getImage(), getPosition().getX(), getPosition().getY(),
+				(int) Constants.SCREEN_SIZE.getWidth() * 3 / 100, (int) Constants.SCREEN_SIZE.getWidth() * 3 / 100,
+				null);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		play.remove(stain);
 	}
-	
-}
 
+}
