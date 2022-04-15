bdce7938fafd0797667356e5b8c5846323bdadf9
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-15 14:21:07 +0200

    Added JavaDocs comments for PricedSkin class

--- src/main/utilities/PricedSkin.java
+++ src/main/utilities/PricedSkin.java
@@ -2,8 +2,17 @@ package main.utilities;
 
 import java.awt.Image;
 
+/**
+ * A class that extends PricedItem class and shapes a Skin object equipped with
+ * a price field
+ */
 public class PricedSkin extends PricedItem {
 
+	/**
+	 * @param name  The PricedSkin object name
+	 * @param image The PricedSkin object image
+	 * @param price The PricedSkin object price
+	 */
 	public PricedSkin(String name, Image image, int price) {
 		super(name, image, price);
 	}
