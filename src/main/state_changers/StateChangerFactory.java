2ba6e89d74bb4faf1ae2f7bb280aa2f6c107abe7
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-15 22:50:19 +0200

    completed StateChangerFactory interface

--- src/main/state_changers/StateChangerFactory.java
+++ src/main/state_changers/StateChangerFactory.java
@@ -4,6 +4,8 @@ import main.utilities.Position;
 import main.utilities.Skin;
 
 public interface StateChangerFactory {
+	
 	public Malus MalusFactory(Position position, Skin skin);
-	public Booster BoosterFactory();
+
+	public Booster BoosterFactory(Position position, Skin skin);
 }
