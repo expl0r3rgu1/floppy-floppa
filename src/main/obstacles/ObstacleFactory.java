4e3471cf81d919344a69a04c2b7d628c3b0c5e0f
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-15 22:20:32 +0200

    created ObstacleFactoryImpl that implements ObstacleFactory

--- src/main/obstacles/ObstacleFactory.java
+++ src/main/obstacles/ObstacleFactory.java
@@ -4,9 +4,9 @@ import main.utilities.Position;
 import main.utilities.Skin;
 
 public interface ObstacleFactory {
-	
+
 	public FixedObstacle fixedObstacleFactory(Position position, Skin skin);
-	
+
 	public MovingObstacle movingObstacleFactory(Position position, Skin skin);
-	
+
 }
