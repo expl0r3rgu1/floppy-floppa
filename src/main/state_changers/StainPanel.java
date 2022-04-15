9f8153af8624d0f6de254ea3e02298e5c452dabb
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-15 13:52:55 +0200

    changed SIZE to Constants.SCREEN_SIZE

--- src/main/state_changers/StainPanel.java
+++ src/main/state_changers/StainPanel.java
@@ -10,32 +10,34 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-class StainPanel extends JPanel{
-	
-	public static final Dimension SIZE = Toolkit.getDefaultToolkit().getScreenSize();
+import main.utilities.Constants;
+
+class StainPanel extends JPanel {
+
 	private Image image;
-	
+
 	public StainPanel(String imageName) {
-		
+
 		this.setLayout(null);
-		
+
 		try {
-			image = ImageIO.read(getClass().getResource("/resources/images/"+imageName+".png"));
+			image = ImageIO.read(getClass().getResource("/resources/images/" + imageName + ".png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		this.setPreferredSize(SIZE);
+
+		this.setPreferredSize(Constants.SCREEN_SIZE);
 		this.setOpaque(false);
-		
+
 		this.setVisible(true);
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D canvas = (Graphics2D) g;
-		canvas.drawImage(image, 0, 0, (int) SIZE.getWidth(), (int) SIZE.getHeight(), null);
+		canvas.drawImage(image, 0, 0, (int) Constants.SCREEN_SIZE.getWidth(), (int) Constants.SCREEN_SIZE.getHeight(),
+				null);
 	}
-	
+
 }
