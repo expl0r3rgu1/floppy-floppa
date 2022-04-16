0063e848d2ddbad880b364164d49d6eee3b0a00e
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-16 14:30:03 +0200

    Removed unused field in TimedObstacleGenerator

--- src/main/infinite_map/TimedObstacleGenerator.java
+++ src/main/infinite_map/TimedObstacleGenerator.java
@@ -8,18 +8,12 @@ import main.utilities.Constants;
 
 public abstract class TimedObstacleGenerator implements ActionListener {
 	private Timer timer;
-	private double obstacleSpeed;
 	private Map map;
 
 	public TimedObstacleGenerator(Map map, double obstacleSpeed) {
 		this.map = map;
-		this.obstacleSpeed = obstacleSpeed;
 		this.timer = new Timer((int) (1000 / (Constants.SPEED * obstacleSpeed)), this);
 		this.timer.start();
-	}
-
-	protected void setObstacleSpeed(double obstacleSpeed) {
-		this.obstacleSpeed = obstacleSpeed;
 	}
 
 	protected Map getMap() {
