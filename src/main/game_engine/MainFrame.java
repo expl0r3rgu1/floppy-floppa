3d21aad6680ec83f2426c8faf49c5895ff74bf3a
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-16 20:17:22 +0200

    Created MainFrame constructor to set the JFrame to fullscreen with Floppy-Floppa as window title bar

--- src/main/game_engine/MainFrame.java
+++ src/main/game_engine/MainFrame.java
@@ -1,5 +1,20 @@
 package main.game_engine;
 
-public class MainFrame extends JFrame{
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	private GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
+
+	public MainFrame() {
+		super("Floppy-Floppa");
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setUndecorated(true);
+		device.setFullScreenWindow(this);
+		this.setVisible(true);
+	}
 
 }
