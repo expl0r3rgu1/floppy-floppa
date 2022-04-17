3bba94381c6ad2628c6156c3876686bd0c19e645
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-17 14:54:00 +0200

    Created basic PlayPanel constructor

--- src/main/game_engine/PlayPanel.java
+++ src/main/game_engine/PlayPanel.java
@@ -2,6 +2,7 @@ package main.game_engine;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,11 +10,19 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 import main.infinite_map.Map;
+import main.utilities.Constants;
+import main.utilities.GameSettings;
 
-public class PlayPanel extends JPanel implements ActionListener{
+public class PlayPanel extends JPanel implements ActionListener {
 	private Timer refreshRate;
 	private Map map;
-	
+
+	public PlayPanel(GameSettings gameSettings) {
+		this.setLayout(new GridBagLayout());
+		this.setPreferredSize(Constants.SCREEN_SIZE);
+
+	}
+
 	@Override
 	public void paintComponent(Graphics g) {
 		Graphics2D canvas = (Graphics2D) g;
