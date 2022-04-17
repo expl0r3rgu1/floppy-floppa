61653ee41faddb8afe4b51293c5100cb96489d6d
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-17 15:25:36 +0200

    Created title GraphicJLabel

--- src/main/game_engine/NicknamePanel.java
+++ src/main/game_engine/NicknamePanel.java
@@ -2,11 +2,13 @@ package main.game_engine;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagLayout;
 
 import javax.swing.JPanel;
 
 import main.utilities.CommonMethods;
+import main.utilities.GraphicJLabel;
 
 public class NicknamePanel extends JPanel {
 	public NicknamePanel(PlayPanel playPanel, GameSettings gameSettings) {
@@ -15,5 +17,10 @@ public class NicknamePanel extends JPanel {
 				new Dimension(CommonMethods.getPixelsFromPercentage(33), CommonMethods.getPixelsFromPercentage(20)));
 		this.setBackground(Color.decode("#80CEE1"));
 		this.setOpaque(true);
+
+		// COMPONENTS TO ENTER THE NICKNAME
+		GraphicJLabel title = new GraphicJLabel("Enter your nickname", new Color(0, 0, 0, 0), new Color(0, 0, 0, 0),
+				"Arial", Font.BOLD);
+		title.setOpaque(false);
 	}
 }
