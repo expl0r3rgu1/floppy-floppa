de9c2162b4eecf5d346534ad8325018e28a0f9ec
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-17 14:55:01 +0200

    Added Map field initialization in constructor

--- src/main/game_engine/PlayPanel.java
+++ src/main/game_engine/PlayPanel.java
@@ -5,13 +5,19 @@ import java.awt.Graphics2D;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 import main.infinite_map.Map;
+import main.infinite_map.ScrollingBackground;
+import main.obstacles.FixedObstacle;
+import main.obstacles.MovingObstacle;
+import main.utilities.CommonMethods;
 import main.utilities.Constants;
 import main.utilities.GameSettings;
+import main.utilities.Skin;
 
 public class PlayPanel extends JPanel implements ActionListener {
 	private Timer refreshRate;
@@ -20,6 +26,10 @@ public class PlayPanel extends JPanel implements ActionListener {
 	public PlayPanel(GameSettings gameSettings) {
 		this.setLayout(new GridBagLayout());
 		this.setPreferredSize(Constants.SCREEN_SIZE);
+		map = new Map(new ScrollingBackground("Default", "Classic"),
+				new FixedObstacle(null, new Skin("pipe", CommonMethods.getImageResource("pipe"))),
+				List.of(new MovingObstacle(null, new Skin("Bingus", CommonMethods.getImageResource("Bingus"))),
+						new MovingObstacle(null, new Skin("Walter", CommonMethods.getImageResource("Walter")))));
 
 	}
 
