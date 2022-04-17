ba2f424079a2f628691b78f11404ce6cbf010554
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-17 14:47:53 +0200

    PlayPanel now implements ActionListener to be able to control frame update rate

--- src/main/game_engine/PlayPanel.java
+++ src/main/game_engine/PlayPanel.java
@@ -1,5 +1,15 @@
 package main.game_engine;
 
-public class PlayPanel {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class PlayPanel extends JPanel implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
 
 }
