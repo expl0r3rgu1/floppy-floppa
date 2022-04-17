e5e12d2e8b005a09e119c3db27e4a54eea9704a7
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-17 15:24:41 +0200

    Created basic constructor for NicknamePanel JPanel

--- src/main/game_engine/NicknamePanel.java
+++ src/main/game_engine/NicknamePanel.java
@@ -1,7 +1,19 @@
 package main.game_engine;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+
 import javax.swing.JPanel;
 
-public class NicknamePanel extends JPanel {
+import main.utilities.CommonMethods;
 
+public class NicknamePanel extends JPanel {
+	public NicknamePanel(PlayPanel playPanel, GameSettings gameSettings) {
+		this.setLayout(new GridBagLayout());
+		this.setPreferredSize(
+				new Dimension(CommonMethods.getPixelsFromPercentage(33), CommonMethods.getPixelsFromPercentage(20)));
+		this.setBackground(Color.decode("#80CEE1"));
+		this.setOpaque(true);
+	}
 }
