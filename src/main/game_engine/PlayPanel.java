3e544103166389c7ef8255762fad280780712212
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-17 14:50:01 +0200

    Added Timer and Map fields to PlayPanel

--- src/main/game_engine/PlayPanel.java
+++ src/main/game_engine/PlayPanel.java
@@ -4,8 +4,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import main.infinite_map.Map;
 
 public class PlayPanel extends JPanel implements ActionListener{
+	private Timer refreshRate;
+	private Map map;
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
