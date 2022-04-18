b73e27933f4bc8bd63ff52d9087f7ce5d5d89806
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-18 16:09:49 +0200

    Now game speed depends on the width of the screen

--- src/main/utilities/Constants.java
+++ src/main/utilities/Constants.java
@@ -5,7 +5,7 @@ import java.awt.Toolkit;
 
 public class Constants {
 	public static final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
-	public static final int SPEED = 60;
+	public static final int SPEED = CommonMethods.getPixelsFromPercentage(6);
 	public static final double FIXED_OBSTACLE_SPEED = 0.0017;
 	public static final double MOVING_OBSTACLE_SPEED = 0.0005;
 	public static final String SAVINGS_FILE_PATH = "savings";
