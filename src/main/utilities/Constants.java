b561eda5148772e94aae708e89caf3c5b69c261e
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-18 23:59:04 +0200

    Introduced a horizontal and a vertical movingFactor constant based on SCREEN_SIZE

--- src/main/utilities/Constants.java
+++ src/main/utilities/Constants.java
@@ -5,7 +5,9 @@ import java.awt.Toolkit;
 
 public class Constants {
 	public static final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
-	public static final int SPEED = CommonMethods.getPixelsFromPercentage(6);
+	public static final int HORIZONTAL_MOVING_FACTOR = (int) (SCREEN_SIZE.getWidth() / 1000);
+	public static final int VERTICAL_MOVING_FACTOR = (int) (SCREEN_SIZE.getHeight() / 1000);
+	public static final int SPEED = 120;
 	public static final double FIXED_OBSTACLE_SPEED = 0.002;
 	public static final double MOVING_OBSTACLE_SPEED = 0.0005;
 	public static final String SAVINGS_FILE_PATH = "savings";
