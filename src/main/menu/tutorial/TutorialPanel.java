cfd3eeee6c3eb786e429035c0795c4604e0cfa54
criicchio <cristina.zoccola01@gmail.com>
2022-04-19 20:56:55 +0200

    Added fields

--- src/main/menu/tutorial/TutorialPanel.java
+++ src/main/menu/tutorial/TutorialPanel.java
@@ -2,6 +2,10 @@ package main.menu.tutorial;
 
 import javax.swing.JPanel;
 
+import main.menu.MainMenu;
+
 public class TutorialPanel extends JPanel{
+	private static final long serialVersionUID = 3621436339823195193L;
+	private MainMenu mainMenu;
 	
 }
