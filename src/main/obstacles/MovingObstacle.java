3be279b38af44715b32cbca0ace59e6368567f46
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-20 00:07:03 +0200

    Removed movingPattern methods

--- src/main/obstacles/MovingObstacle.java
+++ src/main/obstacles/MovingObstacle.java
@@ -42,54 +42,25 @@ public class MovingObstacle extends Movable implements ActionListener {
 		this.timer = new Timer(Constants.CHANGE_DIRECTION_TIMEOUT, this);
 	}
 
+	/**
+	 * @return the MovingObstacle Skin
+	 */
 	public Skin getSkin() {
 		return skin;
 	}
 
+	/**
+	 * Sets the MovingObstacle Skin
+	 * 
+	 * @param skin 
+	 */
 	public void setSkin(Skin skin) {
 		this.skin = skin;
 	}
 
-	/**
-	 * This method changes the Obstacle X and Y positions through time
-	 */
-	public void movingPattern() {
-		this.movingPatternSupportX();
-		if (counter == 0) {
+	
 
-			this.movingPatternSupportY(this.firstPositionShift, this.upShift);
-			counter++;
-
-		} else {
-
-			this.movingPatternSupportY(this.PositionShift, this.downShift);
-
-			this.movingPatternSupportY(this.PositionShift, this.upShift);
-		}
-	}
-
-	/**
-	 * It sets a new position for the object, where its X position is decreased by 3
-	 * pixels, so that the moving obstacle keeps moving from right to left
-	 */
-	private void movingPatternSupportX() {
-		setPosition(new Position(getPosition().getX() - 3 * Constants.MOVING_FACTOR, getPosition().getY()));
-	}
-
-	/**
-	 * For end times the method calls a timer, after
-	 * Constants.CHANGE_DIRECTION_TIMEOUT milliseconds the actionPermormed method is
-	 * called and the Y position changes according to the shift field
-	 * 
-	 * @param end
-	 * @param shift Determines if the position will increment or decrement
-	 */
-	private void movingPatternSupportY(int end, int shift) {
-		for (int i = 0; i < end; i++) {
-			this.shift = shift;
-			this.timer.start();
-		}
-	}
+	
 
 	/**
 	 * {@inheritDoc}
