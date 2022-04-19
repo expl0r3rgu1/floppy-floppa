882be0df9e7db3ed8fffc95aa937a68a6be76c6c
criicchio <cristina.zoccola01@gmail.com>
2022-04-19 20:57:23 +0200

    Added the constructor

--- src/main/menu/tutorial/TutorialPanel.java
+++ src/main/menu/tutorial/TutorialPanel.java
@@ -1,11 +1,21 @@
 package main.menu.tutorial;
 
+import java.awt.GridBagLayout;
+
 import javax.swing.JPanel;
 
 import main.menu.MainMenu;
+import main.utilities.Constants;
 
 public class TutorialPanel extends JPanel{
 	private static final long serialVersionUID = 3621436339823195193L;
 	private MainMenu mainMenu;
+	
+	public TutorialPanel(MainMenu mainMenu) {
+		this.mainMenu = mainMenu;
+		this.setPreferredSize(Constants.SCREEN_SIZE);
+		this.setOpaque(false);
+		this.setLayout(new GridBagLayout());
+	}
 	
 }
