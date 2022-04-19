d12fc111bdf7395a52837137986683154b494469
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-20 00:59:50 +0200

    added Javadoc to StateChangerFactoryImpl

--- src/main/state_changers/StateChangerFactoryImpl.java
+++ src/main/state_changers/StateChangerFactoryImpl.java
@@ -3,6 +3,9 @@ package main.state_changers;
 import main.utilities.Position;
 import main.utilities.Skin;
 
+/**
+ * StateChangerFactoryImpl is a class that implements the interface StateChangerFactory
+ */
 public class StateChangerFactoryImpl implements StateChangerFactory {
 
 	@Override
