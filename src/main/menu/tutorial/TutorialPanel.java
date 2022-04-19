9b9058ac41d334276379b2bc07eed5c60f95be60
criicchio <cristina.zoccola01@gmail.com>
2022-04-19 21:19:56 +0200

    Created addMenuButton() method

--- src/main/menu/tutorial/TutorialPanel.java
+++ src/main/menu/tutorial/TutorialPanel.java
@@ -1,5 +1,7 @@
 package main.menu.tutorial;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.Image;
 
@@ -11,6 +13,7 @@ import main.menu.MainMenu;
 import main.utilities.CommonMethods;
 import main.utilities.Constants;
 import main.utilities.GBCSimplified;
+import main.utilities.GraphicJButton;
 
 public class TutorialPanel extends JPanel {
 	private static final long serialVersionUID = 3621436339823195193L;
@@ -37,7 +40,19 @@ public class TutorialPanel extends JPanel {
 				(int) Constants.SCREEN_SIZE.getHeight() - CommonMethods.getPixelsFromPercentage(10),
 				Image.SCALE_DEFAULT));
 		JLabel tutorialLabel = new JLabel(tutorialIcon);
-		
+
 		this.add(tutorialLabel, new GBCSimplified(0, 1, 0, 0));
+	}
+
+	private void addMenuButton() {
+		GraphicJButton menuButton = new GraphicJButton("MENU", Color.decode("#FFDD62"), Color.decode("#FF971A"),
+				"Arial", Font.BOLD);
+
+		menuButton.addActionListener(e -> {
+			mainMenu.showCard(Constants.PANEL.MENU);
+		});
+
+		this.add(menuButton, new GBCSimplified(0, 2, CommonMethods.getPixelsFromPercentage(25),
+				CommonMethods.getPixelsFromPercentage(1)));
 	}
 }
