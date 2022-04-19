d11a678958db6581f3e2ee64017603205b399487
criicchio <cristina.zoccola01@gmail.com>
2022-04-19 21:03:06 +0200

    Created addTitleLabel() method

--- src/main/menu/tutorial/TutorialPanel.java
+++ src/main/menu/tutorial/TutorialPanel.java
@@ -2,20 +2,30 @@ package main.menu.tutorial;
 
 import java.awt.GridBagLayout;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import main.menu.MainMenu;
+import main.utilities.CommonMethods;
 import main.utilities.Constants;
+import main.utilities.GBCSimplified;
 
-public class TutorialPanel extends JPanel{
+public class TutorialPanel extends JPanel {
 	private static final long serialVersionUID = 3621436339823195193L;
 	private MainMenu mainMenu;
-	
+
 	public TutorialPanel(MainMenu mainMenu) {
 		this.mainMenu = mainMenu;
 		this.setPreferredSize(Constants.SCREEN_SIZE);
 		this.setOpaque(false);
 		this.setLayout(new GridBagLayout());
 	}
-	
+
+	private void addTitleLabel() {
+		JLabel titleLabel = new JLabel("TUTORIAL");
+		titleLabel.setFont(CommonMethods.getFontResource("pixel.TTF").deriveFont(50f));
+
+		this.add(titleLabel, new GBCSimplified(0, 0, 0, CommonMethods.getPixelsFromPercentage(1)));
+	}
+
 }
