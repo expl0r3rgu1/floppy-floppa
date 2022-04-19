422618439c8b29830fa7e9883525b7ab41168ada
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-20 00:03:00 +0200

    updated class MainMenu

--- src/main/menu/MainMenu.java
+++ src/main/menu/MainMenu.java
@@ -2,9 +2,20 @@ package main.menu;
 
 import javax.swing.JPanel;
 
+import main.game_engine.PlayPanel;
+import main.menu.leaderboard.LeaderboardPanel;
+import main.menu.shop.ShopGUI;
+import main.utilities.CommonMethods;
 import main.utilities.Constants;
+import main.utilities.Constants.PANEL;
+import main.utilities.GameSettings;
 
 import java.awt.CardLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * MainMenu is the container for CardLayout
@@ -18,29 +29,28 @@ public class MainMenu extends JPanel implements Menu {
 	private final ShopGUI shopGUI;
 	private final EOGMenuGUI EOGMenuGUI;
 	private final Tutorial tutorial;
-	private final selectionPanel selectionPanel;
+	private final SkinSelectionPanel selectionPanel;
 
-	public MainMenu() {
+	public MainMenu(GameSettings gameSettings) {
 
 		menuPanel = new MenuPanel(this);
 		leaderboardPanel = new LeaderboardPanel(this);
-		playPanel = new PlayPanel(gameSettings);
+		playPanel = new PlayPanel(this, gameSettings);
 		shopGUI = new ShopGUI(this);
 		EOGMenuGUI = new EOGMenuGUI(this);
 		tutorial = new Tutorial(this);
 
-		File savings = new File(Constants.SAVINGS_FILE_PATH);
+		File savingsFile = new File(Constants.SAVINGS_FILE_PATH);
 		if (!savingsFile.exists()) {
-			showCard(PANEL.TUTORIAL);
+			// showCard(PANEL.TUTORIAL);
 			try {
 				savingsFile.createNewFile();
+				FileWriter savingsFileWriter = new FileWriter(savingsFile);
+				savingsFileWriter.write(Constants.SAVINGS_FILE_START_CONTENT);
+				savingsFileWriter.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-
-			FileWriter savingsFileWriter = new FileWriter(savingsFile);
-			savingsFileWriter.write(Constants.SAVINGS_FILE_START_CONTENT);
-			savingsFileWriter.close();
 		}
 
 		this.setLayout(cardLayout);
@@ -55,6 +65,13 @@ public class MainMenu extends JPanel implements Menu {
 
 	}
 
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D canvas = (Graphics2D) g;
+		canvas.drawImage(CommonMethods.getImageResource("Background"), 0, 0, (int) Constants.SCREEN_SIZE.getWidth(),
+				(int) Constants.SCREEN_SIZE.getHeight(), null);
+	}
+	
 	/**
 	 * showCard is a method that when called shows the JPanel panel
 	 * 
@@ -63,5 +80,28 @@ public class MainMenu extends JPanel implements Menu {
 	public void showCard(Constants.PANEL panel) {
 		cardLayout.show(this, panel.name());
 	}
+	
+	public void updateSavings() {
+		try {
+			shopGUI.getShop().fileUpdate();
+			leaderboardPanel.getLeaderboard().writeToFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
+	public void clearSavings() {
+		File savingsFile = new File(Constants.SAVINGS_FILE_PATH);
+		try {
+			FileWriter savingsFileWriter = new FileWriter(savingsFile);
+			savingsFileWriter.write(Constants.SAVINGS_FILE_START_CONTENT);
+			savingsFileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		this.leaderboardPanel.getLeaderboard().clearLeaderboard();
+		this.shopGUI.getShop().clearSavings();
+	}
+	
 }
