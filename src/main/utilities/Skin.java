2d6083ddc94dbff8710f83a65861ad0e817e71e0
criicchio <cristina.zoccola01@gmail.com>
2022-04-20 02:25:29 +0200

    Added equals method in Skin

--- src/main/utilities/Skin.java
+++ src/main/utilities/Skin.java
@@ -7,8 +7,8 @@ public class Skin {
 	private Image image;
 	private int width;
 	private int height;
-	
-	public Skin(String name, Image image, int width, int height){
+
+	public Skin(String name, Image image, int width, int height) {
 		this.name = name;
 		this.image = image;
 		this.width = width;
@@ -30,20 +30,28 @@ public class Skin {
 	public void setImage(Image image) {
 		this.image = image;
 	}
-	
+
 	public int getWidth() {
 		return this.width;
 	}
-	
+
 	public void setWidth(int width) {
 		this.width = width;
 	}
-	
+
 	public int getHeight() {
 		return this.height;
 	}
-	
+
 	public void setHeight(int height) {
 		this.height = height;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Skin other = (Skin) obj;
+
+		return this.name.equals(other.getName()) && this.image.equals(other.getImage())
+				&& this.width == other.getWidth() && this.height == other.getHeight();
 	}
 }
