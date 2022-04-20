f634e563cbed3d71b97d1f12b0ef7db93c9a53e6
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-20 18:06:35 +0200

    Added STATE_CHANGER_SPEED constant

--- src/main/utilities/Constants.java
+++ src/main/utilities/Constants.java
@@ -9,6 +9,7 @@ public class Constants {
 	public static final int SPEED = 60;
 	public static final double FIXED_OBSTACLE_SPEED = 0.0025;
 	public static final double MOVING_OBSTACLE_SPEED = 0.0015;
+	public static final double STATE_CHANGER_SPEED = 0.0007;
 	public static final String SAVINGS_FILE_PATH = "savings";
 	public static final String SAVINGS_FILE_START_CONTENT = "0\n0,0,0,0,0\n0,0,0,0,0";
 	public static final int CHANGED_STATE_TIME = 3000;
