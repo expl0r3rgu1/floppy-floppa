0d19b3d7a0a110c4069a0e5c441de045b6a928e4
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-20 18:05:34 +0200

    Added MOVING_OBSTACLE_SPEED andFIXED_OBSTACLE_SPEE constants

--- src/main/utilities/Constants.java
+++ src/main/utilities/Constants.java
@@ -6,10 +6,9 @@ import java.awt.Toolkit;
 public class Constants {
 	public static final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
 	public static final int MOVING_FACTOR = (int) Math.floor(SCREEN_SIZE.getWidth() / (double) 1000) * 2;
-	public static final double SPACE_BETWEEN_PIPES = (Toolkit.getDefaultToolkit().getScreenSize().getHeight()) / 3;
 	public static final int SPEED = 60;
-	public static final double FIXED_OBSTACLE_SPEED = 0.002;
-	public static final double MOVING_OBSTACLE_SPEED = 0.0005;
+	public static final double FIXED_OBSTACLE_SPEED = 0.0025;
+	public static final double MOVING_OBSTACLE_SPEED = 0.0015;
 	public static final String SAVINGS_FILE_PATH = "savings";
 	public static final String SAVINGS_FILE_START_CONTENT = "0\n0,0,0,0,0\n0,0,0,0,0";
 	public static final int CHANGED_STATE_TIME = 3000;
