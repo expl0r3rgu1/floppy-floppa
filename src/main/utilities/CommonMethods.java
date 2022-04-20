4dcbfa0a97b453d1d2bfe3b775c90053b3fd138d
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-20 17:47:19 +0200

    Removed wrong imports

--- src/main/utilities/CommonMethods.java
+++ src/main/utilities/CommonMethods.java
@@ -11,7 +11,6 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 
 public class CommonMethods {
 	// Utility method to generate random integer in range (consider moving it)
