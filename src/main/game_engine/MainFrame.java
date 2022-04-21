4357917dfb50ffee574195f4cc333444cce6389e
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-21 02:34:30 +0200

    Moved GameSettings field from MainFrame to MainMenu

--- src/main/game_engine/MainFrame.java
+++ src/main/game_engine/MainFrame.java
@@ -18,13 +18,11 @@ import main.utilities.GameSettings;
 public class MainFrame extends JFrame {
 	private GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
 	private final MainMenu mainMenu;
-	private final GameSettings gameSettings;
 
 	public MainFrame() {
 		super("Floppy-Floppa");
 
-		this.gameSettings = new GameSettings();
-		this.mainMenu = new MainMenu(this.gameSettings);
+		this.mainMenu = new MainMenu();
 
 		this.add(mainMenu);
 
