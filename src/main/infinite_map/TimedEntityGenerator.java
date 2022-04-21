bfce8c71f9567c5b62d8be5d4a97ab9562275cca
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-21 03:55:14 +0200

    Initialized fields in constructor setting the timer based on game speed and entity speed

--- src/main/infinite_map/TimedEntityGenerator.java
+++ src/main/infinite_map/TimedEntityGenerator.java
@@ -4,9 +4,16 @@ import java.awt.event.ActionListener;
 
 import javax.swing.Timer;
 
+import main.utilities.Constants;
+
 public abstract class TimedEntityGenerator implements ActionListener {
 	private Timer timer;
 	private Map map;
 	private final double speed;
 
+	public TimedEntityGenerator(Map map, double speed) {
+		this.map = map;
+		this.speed = speed;
+		timer = new Timer((int) ((1000 / Constants.SPEED) / speed), this);
+	}
 }
