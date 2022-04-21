080cdf4918e204c810e45d5c722967b9929b100b
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-21 03:53:58 +0200

    Added basic fields

--- src/main/infinite_map/TimedEntityGenerator.java
+++ src/main/infinite_map/TimedEntityGenerator.java
@@ -2,6 +2,11 @@ package main.infinite_map;
 
 import java.awt.event.ActionListener;
 
+import javax.swing.Timer;
+
 public abstract class TimedEntityGenerator implements ActionListener {
+	private Timer timer;
+	private Map map;
+	private final double speed;
 
 }
