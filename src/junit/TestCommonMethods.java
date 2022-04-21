8767033a207e664fdb8a1fbc71ee93eeaa2ee870
criicchio <cristina.zoccola01@gmail.com>
2022-04-21 21:51:31 +0200

    Changed getPixelsFromPercentageTest()

--- src/junit/TestCommonMethods.java
+++ src/junit/TestCommonMethods.java
@@ -15,16 +15,11 @@ public class TestCommonMethods {
 
 	final private int NUM_MIN = 0;
 	final private int NUM_MAX = 10;
-	final private int PERCENTAGE = 10;
-	final private int SCREEN_WIDTH = 1536;
-	final private int SCREEN_HEIGHT = 864;
-	final private int PIXELS_WIDTH_RESULT = 153;
-	final private int PIXELS_HEIGHT_RESULT = 86;
+	final private int PERCENTAGE = 100;
 	final private String IMAGE_NAME = "Background";
-	final private String FONT_NAME = "pixel.TTF";
+	final private String FONT_NAME = "pixel";
 	final private String GIF_NAME = "Tutorial";
-	
-	
+
 	@Test
 	/**
 	 * Checks if the number is actually in the given range
@@ -33,20 +28,16 @@ public class TestCommonMethods {
 		int randomNum = CommonMethods.getRandomNumber(NUM_MIN, NUM_MAX);
 		assertTrue(randomNum <= NUM_MAX && randomNum >= NUM_MIN);
 	}
-	
+
 	@Test
 	/**
-	 * Checks if those methods calculate the right amount of pixels;
-	 * Considering that every screen has a different size, the results of this test are 
-	 * valid only if the width of the screen is 1536 and the height is 864
+	 * Checks if those methods calculate the right amount of pixels
 	 */
 	void getPixelsFromPercentageTest() {
-		if (Constants.SCREEN_SIZE.equals(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT))) {
-			assertTrue(CommonMethods.getPixelsFromPercentageHeight(PERCENTAGE) == PIXELS_HEIGHT_RESULT);
-			assertTrue(CommonMethods.getPixelsFromPercentageWidth(PERCENTAGE) == PIXELS_WIDTH_RESULT);
-		}
+		assertTrue(Constants.SCREEN_SIZE.equals(new Dimension(CommonMethods.getPixelsFromPercentageWidth(PERCENTAGE),
+				CommonMethods.getPixelsFromPercentageHeight(PERCENTAGE))));
 	}
-	
+
 	@Test
 	/**
 	 * Checks if those methods actually gets the resource
