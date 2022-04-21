37f723c510dc25bc15964d8ec81edf8ea3466985
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-21 04:20:06 +0200

    Switched order of constructors from biggest to smallest

--- src/main/utilities/GBCSimplified.java
+++ src/main/utilities/GBCSimplified.java
@@ -7,15 +7,6 @@ public class GBCSimplified extends GridBagConstraints {
 
 	private static final long serialVersionUID = 3822190102776934506L;
 
-	public GBCSimplified(int gridx, int gridy, int ipadx, int ipady, Insets insets) {
-		super();
-		this.gridx = gridx;
-		this.gridy = gridy;
-		this.ipadx = ipadx;
-		this.ipady = ipady;
-		this.insets = insets;
-	}
-
 	public GBCSimplified(int gridx, int gridy, int ipadx, int ipady, Insets insets, int anchor) {
 		super();
 		this.gridx = gridx;
@@ -24,6 +15,15 @@ public class GBCSimplified extends GridBagConstraints {
 		this.ipady = ipady;
 		this.insets = insets;
 		this.anchor = anchor;
+	}
+
+	public GBCSimplified(int gridx, int gridy, int ipadx, int ipady, Insets insets) {
+		super();
+		this.gridx = gridx;
+		this.gridy = gridy;
+		this.ipadx = ipadx;
+		this.ipady = ipady;
+		this.insets = insets;
 	}
 
 	public GBCSimplified(int gridx, int gridy, int ipadx, int ipady) {
