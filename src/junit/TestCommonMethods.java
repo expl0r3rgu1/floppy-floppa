9e0d06a6fc08da81d9d2d4f6343f06502de0bb04
criicchio <cristina.zoccola01@gmail.com>
2022-04-21 15:40:51 +0200

    Created test for getRandomNumber()

--- src/junit/TestCommonMethods.java
+++ src/junit/TestCommonMethods.java
@@ -1,7 +1,22 @@
 package junit;
 
-import org.junit.jupiter.api.Test; 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import main.utilities.CommonMethods; 
 
 public class TestCommonMethods {
 
+	final private int NUM_MIN = 0;
+	final private int NUM_MAX = 10;
+	
+	@Test
+	/**
+	 * Checks if the number is actually in the given range
+	 */
+	void getRandomNumberTest() {
+		int randomNum = CommonMethods.getRandomNumber(NUM_MIN, NUM_MAX);
+		assertTrue(randomNum <= NUM_MAX && randomNum >= NUM_MIN);
+	}
 }
