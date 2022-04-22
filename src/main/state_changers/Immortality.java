81f054543e5f56f6635dad57fbbf26d800c2e55d
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-22 17:05:58 +0200

    Added nested class to test with JUnit

--- src/main/state_changers/Immortality.java
+++ src/main/state_changers/Immortality.java
@@ -1,10 +1,17 @@
 package main.state_changers;
 
+import static org.junit.Assert.assertTrue;
+
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
+
 import javax.swing.Timer;
 
+import org.junit.jupiter.api.Test;
+
+import main.character.Character;
 import main.utilities.CommonMethods;
 import main.utilities.Constants;
 import main.utilities.Position;
@@ -59,7 +66,7 @@ public class Immortality extends Booster {
 	 */
 	private void updatePositionX() {
 		setPosition(new Position(getPosition().getX() - 3 * Constants.MOVING_FACTOR, getPosition().getY()));
-		}
+	}
 
 	/**
 	 * {@inheritDoc}
@@ -80,5 +87,41 @@ public class Immortality extends Booster {
 				CommonMethods.getPixelsFromPercentageWidth(3), CommonMethods.getPixelsFromPercentageWidth(3), null);
 
 		this.updatePositionX();
+	}
+
+	public static class TestImmortality {
+
+		final private Position POSITION = new Position((int) (Constants.SCREEN_SIZE.getWidth()),
+				(int) Constants.SCREEN_SIZE.getHeight() / 2);
+		final private Position HALFWAY_POSITION = new Position((int) (Constants.SCREEN_SIZE.getWidth() / 2),
+				(int) Constants.SCREEN_SIZE.getHeight() / 2);
+		final private Skin SKIN = new Skin("immortality", CommonMethods.getImageResource("immortality"),
+				(int) this.POSITION.getX(), (int) this.POSITION.getY());
+
+		@Test
+		/**
+		 * Check if the moving pattern of the booster works correctly
+		 */
+		void immortalityBoosterMovement() {
+			Immortality immortality1 = new Immortality(this.POSITION, this.SKIN);
+			immortality1.updatePositionX();
+			assertTrue(immortality1.getPosition().getX() == POSITION.getX() - 3 * Constants.MOVING_FACTOR);
+			assertTrue(immortality1.getPosition().getY() == POSITION.getY());
+
+			Immortality immortality2 = new Immortality(this.HALFWAY_POSITION, this.SKIN);
+			immortality2.updatePositionX();
+			assertTrue(immortality2.getPosition().getX() == HALFWAY_POSITION.getX() - 3 * Constants.MOVING_FACTOR);
+			assertTrue(immortality2.getPosition().getY() == HALFWAY_POSITION.getY());
+		}
+
+		@Test
+		/**
+		 * Checks if the booster actually sets the Immortality field to true
+		 */
+		void coinsIncrementChangeState() {
+			Immortality immortality = new Immortality(this.POSITION, this.SKIN);
+			immortality.changeState();
+			assertTrue(Character.immortal);
+		}
 	}
 }
