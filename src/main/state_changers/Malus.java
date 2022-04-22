c149e0e4aeb9f17de67ac253bed68bcdc1fbb346
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-22 18:10:25 +0200

    made class Malus abstract, made method changeState abstract, eliminated animate and created method moveOffScreen

--- src/main/state_changers/Malus.java
+++ src/main/state_changers/Malus.java
@@ -7,9 +7,10 @@ import main.utilities.Position;
 import main.utilities.Skin;
 
 /**
- * A class that implements an object that hampers the character in different ways
+ * A class that implements an object that hampers the character in different
+ * ways
  */
-public class Malus extends Movable {
+public abstract class Malus extends Movable {
 
 	private Skin skin;
 
@@ -40,13 +41,9 @@ public class Malus extends Movable {
 		this.skin = skin;
 	}
 
-	public Object changeState() {
-		return null;
-		// TO BE IMPLEMENTED IN SUB-CLASSES
-	}
+	public abstract void changeState();
 
-	@Override
-	public void animate(Graphics2D canvas) {
-		// TO BE IMPLEMENTED IN SUB-CLASSES
+	public void moveOffScreen() {
+		setPosition(new Position(-getSkin().getWidth(), getPosition().getY()));
 	}
 }
