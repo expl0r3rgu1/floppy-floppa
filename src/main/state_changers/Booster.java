7c953b8977a27749cae428fb6a80eaa4a238a4be
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-22 15:13:10 +0200

    Added JavaDoc

--- src/main/state_changers/Booster.java
+++ src/main/state_changers/Booster.java
@@ -36,8 +36,15 @@ public abstract class Booster extends Movable {
 		this.skin = skin;
 	}
 
+	/**
+	 * This method simulates the effect of the booster
+	 */
 	public abstract void changeState();
 
+	/**
+	 * Used to move the booster off screen so that there is only one effective
+	 * collision
+	 */
 	public void moveOffScreen() {
 		setPosition(new Position(-getSkin().getWidth(), getPosition().getY()));
 	}
