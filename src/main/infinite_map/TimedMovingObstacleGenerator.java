06ce6ab899925e7868248feff231bfe67ceb2599
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-23 15:03:37 +0200

    Added JavaDoc comments to TimedMovingObstacleGenerator

--- src/main/infinite_map/TimedMovingObstacleGenerator.java
+++ src/main/infinite_map/TimedMovingObstacleGenerator.java
@@ -4,12 +4,24 @@ import java.awt.event.ActionEvent;
 
 import main.utilities.Constants;
 
+/**
+ * TimedEntityGenerator customized for MovingObstacle
+ */
 public class TimedMovingObstacleGenerator extends TimedObstacleGenerator {
 
+	/**
+	 * Initializes the Generator with a speed defined by
+	 * Constants.MOVING_OBSTACLE_SPEED
+	 * 
+	 * @param map The instance of the infinite Map
+	 */
 	public TimedMovingObstacleGenerator(Map map) {
 		super(map, Constants.MOVING_OBSTACLE_SPEED);
 	}
 
+	/**
+	 * Adds a MovingObstacle to the map
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		this.getMap().addMovingObstacle(); // Spawns a MovingObstacle
