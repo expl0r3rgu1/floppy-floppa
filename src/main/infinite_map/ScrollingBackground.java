6b1213816a9a0011ecf2b752340a2c8e23a299ff
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-23 14:53:47 +0200

    Added JavaDoc comments to ScrollingBackground

--- src/main/infinite_map/ScrollingBackground.java
+++ src/main/infinite_map/ScrollingBackground.java
@@ -6,18 +6,33 @@ import java.awt.Image;
 import main.utilities.Constants;
 import main.utilities.Position;
 
+/**
+ * A Decorator of Background that makes of two still Backgrounds a scrollable
+ * one
+ */
 public class ScrollingBackground {
 
 	// Two copies of the background image to scroll
 	private final Background backOne;
 	private final Background backTwo;
 
+	/**
+	 * @param name  The name of the ScrollingBackground
+	 * @param image The image that will be displayed
+	 */
 	public ScrollingBackground(String name, Image image) {
 		backOne = new Background(name, image);
 		backTwo = new Background(name, image, new Position((int) Constants.SCREEN_SIZE.getWidth(), 0));
 	}
 
-	// Animates the two backgrounds
+	/**
+	 * Animates the two Background instances, updates their Position and moves them
+	 * to the right edge of the screen when if Background.isOffStageLeft() returns
+	 * true
+	 * 
+	 * @param canvas A Graphics2D canvas to animate the two Background instances
+	 *               onto
+	 */
 	public void animate(Graphics2D canvas) {
 		backOne.animate(canvas);
 		backTwo.animate(canvas);
