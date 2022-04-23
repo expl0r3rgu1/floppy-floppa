b5bcd8f8f594742a1920df421370d11ca8639147
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-23 14:14:52 +0200

    Removed speed field in TimedEntityGenerator and its setter

--- src/main/infinite_map/TimedEntityGenerator.java
+++ src/main/infinite_map/TimedEntityGenerator.java
@@ -9,20 +9,10 @@ import main.utilities.Constants;
 public abstract class TimedEntityGenerator implements ActionListener {
 	private Timer timer;
 	private Map map;
-	private final double speed;
 
 	public TimedEntityGenerator(Map map, double speed) {
 		this.map = map;
-		this.speed = speed;
 		timer = new Timer((int) ((1000 / Constants.SPEED) / speed), this);
-	}
-
-	public void updateSpeed() {
-		if (timer != null) {
-			timer.stop();
-		}
-		timer = new Timer((int) ((1000 / Constants.SPEED) / this.speed), this);
-		timer.start();
 	}
 
 	public Map getMap() {
