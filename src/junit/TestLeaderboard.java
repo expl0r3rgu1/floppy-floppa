34454e6650bbe3fe42a76dc417f55f2e1acaf27c
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-04-23 23:00:40 +0200

    Created createFile method in TestLeaderboard

---
+++ src/junit/TestLeaderboard.java
@@ -0,0 +1,30 @@
+package junit;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import main.menu.leaderboard.Leaderboard;
+import main.menu.leaderboard.Player;
+import main.utilities.Constants;
+
+public class TestLeaderboard {
+
+	private void createFile() {
+		File savingsFile = new File(Constants.SAVINGS_FILE_PATH);
+
+		try {
+			savingsFile.createNewFile();
+			FileWriter savingsFileWriter = new FileWriter(savingsFile);
+			savingsFileWriter.write(Constants.SAVINGS_FILE_START_CONTENT);
+			savingsFileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
