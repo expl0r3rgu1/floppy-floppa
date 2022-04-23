aa1769b92406f4633dccd2f38ffdb30f08335259
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-23 03:36:43 +0200

    added test for the animate made by FixedObstacle

--- src/main/obstacles/FixedObstacle.java
+++ src/main/obstacles/FixedObstacle.java
@@ -1,6 +1,11 @@
 package main.obstacles;
 
+import static org.junit.Assert.assertTrue;
+
 import java.awt.Graphics2D;
+
+import org.junit.jupiter.api.Test;
+
 import main.utilities.CommonMethods;
 import main.utilities.Constants;
 import main.utilities.Movable;
@@ -54,6 +59,45 @@ public class FixedObstacle extends Movable {
 	 */
 	private void updatePosition() {
 		getPosition().setX(getPosition().getX() - Constants.MOVING_FACTOR);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		FixedObstacle other = (FixedObstacle) obj;
+		return super.equals(other) && this.skin.equals(other.skin);
+	}
+
+	public static class TestFixedObstacle {
+
+		final private Position POSITION = new Position((int) (Constants.SCREEN_SIZE.getWidth()),
+				(int) Constants.SCREEN_SIZE.getHeight() / 2);
+		final private Position HALFWAY_POSITION = new Position((int) (Constants.SCREEN_SIZE.getWidth() / 2),
+				(int) Constants.SCREEN_SIZE.getHeight() / 2);
+		final private Skin SKIN = new Skin("Bingus", CommonMethods.getImageResource("Bingus"),
+				(int) this.POSITION.getX(), (int) this.POSITION.getY());
+		private int direction = -1;
+
+		@Test
+		/**
+		 * Check if the moving pattern of the fixed obstacle works correctly
+		 */
+		public void movingObstacleMovement() {
+			FixedObstacle fixedObstacle1 = new FixedObstacle(this.POSITION, this.SKIN);
+			fixedObstacle1.updatePosition();
+
+			assertTrue(fixedObstacle1.getPosition().getX() == (POSITION.getX() - 3 * Constants.MOVING_FACTOR));
+			assertTrue(fixedObstacle1.getPosition().getY() == (POSITION.getY() + direction * Constants.MOVING_FACTOR));
+
+			FixedObstacle fixedObstacle2 = new FixedObstacle(this.HALFWAY_POSITION, this.SKIN);
+			fixedObstacle2.updatePosition();
+			assertTrue(fixedObstacle2.getPosition().getX() == (HALFWAY_POSITION.getX() - 3 * Constants.MOVING_FACTOR));
+			assertTrue(fixedObstacle2.getPosition()
+					.getY() == (HALFWAY_POSITION.getY() + direction * Constants.MOVING_FACTOR));
+		}
+
 	}
 
 }
